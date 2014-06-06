4d41920748af4ade054ddb1292fc93f4a4aef8a0
roberto <roberto.angeloni@pix4d.com>
2014-06-06 09:12:11 +0200

    moved sources into src directory

--- build.sh
+++ build.sh
@@ -5,9 +5,10 @@ BUILD_DIR=./build
 SRC_DIR=..
 
 mkdir -p $BUILD_DIR
-cd BUILD_DIR
-cmake $SRC_DIR
-make && ./src/dblayer_test dblayer:sqlite:./examples/test.db
-cd ..
+cd ${BUILD_DIR}
+cmake ${SRC_DIR}
+make
+# && ./src/dblayer_test dblayer:sqlite:./examples/test.db
+#cd ..
 
 
