cc2d3e12975cb5f0e6b7f86bedb068137591282f
roberto <roberto.angeloni@pix4d.com>
2014-06-06 14:52:20 +0200

    ...

--- build.sh
+++ build.sh
@@ -2,12 +2,16 @@
 
 
 BUILD_DIR=./build
-SRC_DIR=..
+SRC_DIR=../src
+
+
+cmake_params="-DPROJECT_SOURCE_DIR=${SRC_DIR}"
 
 mkdir -p $BUILD_DIR
 cd ${BUILD_DIR}
-cmake ${SRC_DIR}
-make
+#echo "cmake ${cmake_params} ${SRC_DIR}"
+cmake ${cmake_params} ${SRC_DIR}
+make -j 12
 # && ./src/dblayer_test dblayer:sqlite:./examples/test.db
 #cd ..
 
