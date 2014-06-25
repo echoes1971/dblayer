33e4644515cb9c584d820d27d0fb2c2e572abc29
roberto <roberto.angeloni@pix4d.com>
2014-06-25 17:03:26 +0200

    Qt5 stuff: quite messy and not compiling

--- build.sh
+++ build.sh
@@ -3,18 +3,22 @@
 
 BUILD_DIR=./build
 SRC_DIR=../src
+PATH=/home/roberto/Qt/5.3/gcc_64/bin/:${PATH}
 
+echo "PATH: ${PATH}"
 
 cmake_params="-DPROJECT_SOURCE_DIR=${SRC_DIR}"
 cmake_params="${cmake_params} -DCMAKE_BUILD_TYPE=Debug "
 #cmake_params="${cmake_params} -DCMAKE_BUILD_TYPE=Release "
+
 
 rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR
 cd ${BUILD_DIR}
 #echo "cmake ${cmake_params} ${SRC_DIR}"
 cmake ${cmake_params} ${SRC_DIR}
-make -j 12 && ./apps/dblayer_test dblayer:sqlite:../examples/test.db
+make && ./apps/dblayer_test dblayer:sqlite:../examples/test.db
+#make -j 12 && ./apps/dblayer_test dblayer:sqlite:../examples/test.db
 cd ..
 
 
